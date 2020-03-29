0d8cc3fd1bc0ed5beead83c47b22d72de5fce454
SAMEER TARIQ <imsamtar@gmail.com>
2020-03-29 18:42:46 +0500

    Accepts prop called every={x} (where x in seconds) and re-mutates after every x seconds

--- components/Mutate.svelte
+++ components/Mutate.svelte
@@ -3,16 +3,27 @@
   import { mutate } from "../graphql/actions";
 
   export let mutation;
+  export let every = undefined;
   export let variables = {};
 
   const dispatch = createEventDispatcher();
-  let graphql, promise;
+  let graphql, promise, response;
 
   onMount(() => {
     promise = mutate(mutation, variables);
-    promise.then(data => {
-      dispatch("response", data);
+    promise.then(resp => {
+      response = resp;
+      dispatch("response", resp);
     });
+    if (every) {
+      setInterval(async () => {
+        let resp = await mutate(mutation, variables);
+        if (JSON.stringify(response) !== JSON.stringify(resp)) {
+          response = resp;
+          dispatch("response", resp);
+        }
+      }, every * 1000);
+    }
   });
 </script>
 
@@ -20,8 +31,8 @@
   {#if promise}
     {#await promise}
       <slot name="loading" />
-    {:then response}
-      <slot {response} />
+    {:then resp}
+      <slot response={response || resp} />
     {:catch error}
       <slot name="error" {error} />
     {/await}
