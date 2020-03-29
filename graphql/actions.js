ac75ae05cdedc9f2c46bcbb94275256edba02dfd
SAMEER TARIQ <imsamtar@gmail.com>
2020-03-29 18:49:36 +0500

    Only use httpLink for query and mutation and both (Websocket and http) links for subscription

--- graphql/actions.js
+++ graphql/actions.js
@@ -3,17 +3,17 @@ import client from ".";
 
 const query = (query, variables = {}) => {
   if (typeof query == "string") query = gql(query);
-  return client().query({ query, variables });
+  return client.query(query, variables);
 };
 
 const subscribe = (query, variables = {}) => {
   if (typeof query == "string") query = gql(query);
-  return client().subscribe({ query, variables });
+  return client.subscribe(query, variables);
 };
 
 const mutate = (mutation, variables = {}) => {
   if (typeof mutation == "string") mutation = gql(mutation);
-  return client().mutate({ mutation, variables });
+  return client.mutate(mutation, variables);
 };
 
 export { query, subscribe, mutate };
--- graphql/index.js
+++ graphql/index.js
@@ -6,37 +6,67 @@ import ApolloClient from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { serverUri as server } from "./store";
 
-export default function() {
-  let serverUri;
-  let token = localStorage.getItem("token");
-  server.subscribe(uri => (serverUri = uri))();
-  if (!serverUri || !token) return 0;
-  const httpLink = new HttpLink({
-    uri: serverUri,
-    headers: { Authorization: `Bearer ${token}` }
-  });
-  const wsLink = new WebSocketLink({
-    uri: `ws://${serverUri.split("//").splice(-1)[0]}`,
-    options: {
-      reconnect: true,
-      connectionParams: {
-        headers: { Authorization: `Bearer ${token}` }
+const graphql = {
+  get serverUri() {
+    let serverUri;
+    server.subscribe(uri => (serverUri = uri))();
+    return serverUri;
+  },
+  get token() {
+    return localStorage.getItem("token");
+  },
+  get httpClient() {
+    if (!graphql.serverUri || !graphql.token) return 0;
+    const link = new HttpLink({
+      uri: graphql.serverUri,
+      headers: { Authorization: `Bearer ${graphql.token}` }
+    });
+    return new ApolloClient({
+      link,
+      cache: new InMemoryCache()
+    });
+  },
+  get client() {
+    if (!graphql.serverUri || !graphql.token) return 0;
+    const httpLink = new HttpLink({
+      uri: graphql.serverUri,
+      headers: { Authorization: `Bearer ${graphql.token}` }
+    });
+    const wsLink = new WebSocketLink({
+      uri: `ws://${graphql.serverUri.split("//").splice(-1)[0]}`,
+      options: {
+        reconnect: true,
+        connectionParams: {
+          headers: { Authorization: `Bearer ${graphql.token}` }
+        }
       }
-    }
-  });
-  const link = split(
-    ({ query }) => {
-      const definition = getMainDefinition(query);
-      return (
-        definition.kind === "OperationDefinition" &&
-        definition.operation === "subscription"
-      );
-    },
-    wsLink,
-    httpLink
-  );
-  return new ApolloClient({
-    link,
-    cache: new InMemoryCache()
-  });
-}
+    });
+    const link = split(
+      ({ query }) => {
+        const definition = getMainDefinition(query);
+        return (
+          definition.kind === "OperationDefinition" &&
+          definition.operation === "subscription"
+        );
+      },
+      wsLink,
+      httpLink
+    );
+    return new ApolloClient({
+      link,
+      cache: new InMemoryCache()
+    });
+  }
+};
+
+export default {
+  query(query, variables) {
+    return graphql.httpClient.query({ query, variables });
+  },
+  subscribe(query, variables) {
+    return graphql.client.subscribe({ query, variables });
+  },
+  mutate(mutation, variables) {
+    return graphql.httpClient.mutate({ mutation, variables });
+  }
+};
