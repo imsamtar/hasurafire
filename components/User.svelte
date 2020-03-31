30dff4f00e43d57a0d786c56c761ac2670cb14e7
SAMEER TARIQ <imsamtar@gmail.com>
2020-03-31 19:03:54 +0500

    Emmits events like on:in, on:out, on:signin and on:signout

--- components/User.svelte
+++ components/User.svelte
@@ -1,20 +1,27 @@
 <script>
-  import { onMount } from "svelte";
+  import { onMount, createEventDispatcher } from "svelte";
   import firebase, { currentUser, loginStatus } from "../auth";
 
+  const dispatch = createEventDispatcher();
   let user, auth;
 
   onMount(() => {
+    let firstTime = true;
     $firebase.auth().onAuthStateChanged(async user => {
       if (user) {
         localStorage.setItem("token", await user.getIdToken(true));
         $currentUser = user;
         $loginStatus = 1;
+        if (!firstTime) dispatch("signin", user);
+        dispatch("in", user);
       } else {
         $currentUser = 0;
         $loginStatus = -1;
         localStorage.removeItem("token");
+        if (!firstTime) dispatch("signout", user);
+        dispatch("out", user);
       }
+      firstTime = false;
     });
     auth = $firebase.auth();
   });
