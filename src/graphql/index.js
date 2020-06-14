00567f69ef4d40e6f3bd438873eff44bf0c335ba
SAMEER TARIQ <imsamtar@gmail.com>
2020-06-14 10:29:06 +0500

    Auto correct type of query e.g. turn query hello {} to subscription hello {} if using subscribe

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -2,23 +2,24 @@ import gql from "graphql-tag";
 import client from "./client";
 import { queries } from "../store";
 
-function parseQuery(query) {
+function parseQuery(query, type) {
   if (typeof query == "string") {
     query &&
       query.indexOf(" ") === -1 &&
       query.indexOf("{") === -1 &&
       queries.subscribe((queries) => (query = queries[query] || query))();
+    query = query.replace(/^\s*(query|mutation|subscription)?\s*/g, `${type} `);
     return gql(query);
   }
   return query;
 }
 
 export const query = (query, variables = {}, options = {}) => {
-  return client.query(parseQuery(query), variables, options)
+  return client.query(parseQuery(query, "query"), variables, options)
 };
 export const subscribe = (query, variables = {}, options = {}) => {
-  return client.subscribe(parseQuery(query), variables, options)
+  return client.subscribe(parseQuery(query, "subscription"), variables, options)
 };
 export const mutate = (mutation, variables = {}, options = {}) => {
-  return client.mutate(parseQuery(mutation), variables, options)
+  return client.mutate(parseQuery(mutation, "mutation"), variables, options)
 };
